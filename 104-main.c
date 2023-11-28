231f34eaeac013b3f7acfda794bbda7d0f614996
sarora2200 <sarora2200@gmail.com>
2023-11-28 10:55:26 -0800

    done

--- 104-main.c
+++ 104-main.c
@@ -16,7 +16,7 @@ root->left = binary_tree_node(root, 64);
 root->left->left = binary_tree_node(root->left, 32);
 binary_tree_print(root);
 printf("Rotate-right %d\n", root->n);
-root = binary_tree_rotate_right(root);
+root = Binary_Tree_Rotate_Right(root);
 binary_tree_print(root);
 printf("\n");
 
@@ -24,7 +24,7 @@ root->left->left = binary_tree_node(root->left, 20);
 root->left->right = binary_tree_node(root->left, 56);
 binary_tree_print(root);
 printf("Rotate-right %d\n", root->n);
-root = binary_tree_rotate_right(root);
+root = Binary_Tree_Rotate_Right(root);
 binary_tree_print(root);
 return (0);
 }
